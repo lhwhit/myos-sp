c70dfcec2bb5c783b048f04a2e8e1e39281b9ac3
lhwhit <lhwhit@users.noreply.github.com>
2018-10-24 11:35:46 +0800

    Add files via upload

--- index-2.php
+++ index-2.php
@@ -2,12 +2,13 @@
 <html>
 <body>
 
-<video width="320" height="240" controls="controls" autoplay="autoplay">
+<video width="100%" height="100%" controls="controls" autoplay="autoplay">
 <source src="01-02.mp4" type="video/mp4" />
 </video>
 
 <?php
-echo "æ“ä½œç³»ç»Ÿæ¦‚å¿µ";
+?
+echo "²Ù×÷ÏµÍ³¸ÅÄî";
 ?>
 
 </body>
